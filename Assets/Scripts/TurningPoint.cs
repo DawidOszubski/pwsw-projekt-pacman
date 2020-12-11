870005604e65092a8cefabfecafb2443bdbe51a8
Naskręt Cezary <ceznas000@utp.edu.pl>
2020-12-11 17:38:05 +0100

    pierwszy commit

---
+++ Assets/Scripts/TurningPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurningPoint : MonoBehaviour
+{
+    public TurningPoint[] nextPoints;
+    public Vector2[] vectToNextPoint;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+}
